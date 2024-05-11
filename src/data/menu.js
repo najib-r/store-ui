4417813deda1a3aeac9370b40b17ba950615a726
najib-r <najib72645@gmail.com>
2024-05-11 11:27:12 +0800

    feat: add placeholder data

---
+++ src/data/menu.js
@@ -0,0 +1,53 @@
+const menu = [
+  { id: 1,
+    name: 'Beef Burrito Regular',
+    img: 'https://www.kedaipay.com/site/product-image?id=488&slide=1&time=1715396398',
+    desc: 'https://www.kedaipay.com/site/product-image?id=488&slide=2&time=1715396398',
+    category: 'Mains',
+    price: 8
+  },
+  { id: 2,
+    name: 'Chicken Burrito Regular',
+    img: 'https://www.kedaipay.com/site/product-image?id=488&slide=1&time=1715396398',
+    desc: 'https://www.kedaipay.com/site/product-image?id=488&slide=2&time=1715396398',
+    category: 'Mains',
+    price: 7.5
+  },
+  { id: 3,
+    name: 'Meal 1',
+    img: 'https://www.kedaipay.com/site/product-image?id=488&slide=1&time=1715396398',
+    desc: 'https://www.kedaipay.com/site/product-image?id=488&slide=2&time=1715396398',
+    category: 'Meal',
+    price: 12
+  },
+  { id: 4,
+    name: 'Meal 2',
+    img: 'https://www.kedaipay.com/site/product-image?id=488&slide=1&time=1715396398',
+    desc: 'https://www.kedaipay.com/site/product-image?id=488&slide=2&time=1715396398',
+    category: 'Meal',
+    price: 9.5
+  },
+  { id: 5,
+    name: 'Meal 3',
+    img: 'https://www.kedaipay.com/site/product-image?id=488&slide=1&time=1715396398',
+    desc: 'https://www.kedaipay.com/site/product-image?id=488&slide=2&time=1715396398',
+    category: 'Meal',
+    price: 10.5
+  },
+  { id: 6,
+    name: 'Drink 1',
+    img: 'https://www.kedaipay.com/site/product-image?id=488&slide=1&time=1715396398',
+    desc: 'https://www.kedaipay.com/site/product-image?id=488&slide=2&time=1715396398',
+    category: 'Drinks',
+    price: 10.5
+  },
+  { id: 7,
+    name: 'Drink 2',
+    img: 'https://www.kedaipay.com/site/product-image?id=488&slide=1&time=1715396398',
+    desc: 'https://www.kedaipay.com/site/product-image?id=488&slide=2&time=1715396398',
+    category: 'Drinks',
+    price: 10.5
+  },
+]
+
+export default menu
---
+++ src/views/Store.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-card v-for="item in menu">
+      {{ item }}
+    </v-card>
+  </div>
+</template>
+
+<script setup>
+import menu from '../data/menu'
+</script>
+
+<style scoped>
+</style>
